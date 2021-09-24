73932c61c198afed06bb28fa27eb25744782d5e5
youlielab <64575096+youlielab@users.noreply.github.com>
2021-09-24 09:40:06 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+$(function(){
+    /テキスト文字色変更/ 
+    $('li').hover(
+        function(){
+            $(this).css('color','red');
+
+        },
+        function(){
+            $(this).css('color','');
+
+        }
+    );
+
+    $('.insta-link p,i').hover(
+        function(){
+            $(this).css('color','red');
+
+        },
+        function(){
+            $(this).css('color','');
+
+        }
+    );
+
+    /リンク先へのスクロール：PC/ 
+    $('.header-logo1,.header-logo').click(function(){
+        $('html,body').animate({
+            'scrollTop':0
+        },'slow');
+    })
+
+    $('#ul a').click(function(){
+        var id =$(this).attr('href');
+        var position= $(id).offset().top;
+        
+        $('html,body').animate({
+            'scrollTop':position
+        },500);
+    });
+
+    /リンク先へのスクロール：スマホ [about buy top]の順/ 
+    $('.sp_menu-about').click(function(){
+        var id=$('#main-wrapper').offset();
+        var position=$('#main-wrapper').offset().top;
+
+        $('html,body').animate({
+            'scrollTop':position
+        },500);
+    })
+
+    $('.sp_menu-2').click(function(){
+        var id=$('#links-wrapper').offset();
+        var position=$('#links-wrapper').offset().top;
+
+        $('html,body').animate({
+            'scrollTop':position
+        },500);
+    })
+
+    $('.sp_menu-3').click(function(){
+        $('html,body').animate({
+            'scrollTop':0
+        },500);
+    })
+
+});
